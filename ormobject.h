14fc29a32e479859a4269a28a0f7ccf50b72ab01
smarttowel <mihadenok_pavel@mail.ru>
2013-03-15 21:57:30 +0300

    Added macro for declaring model's field

--- ormobject.h
+++ ormobject.h
@@ -1,6 +1,13 @@
 #ifndef ORMOBJECT_H
 #define ORMOBJECT_H
 
+#define ORM_PROPERTY(type, name) \
+    Q_PROPERTY(type name READ get##name WRITE set##name) \
+    public: \
+    type name; \
+    type get##name(){ return name; } \
+    void set##name(type input_##name) { name = input_##name; } \
+
 #include <QObject>
 
 class ORMObject : public QObject

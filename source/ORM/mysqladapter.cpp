09ae0e696120acc3faddca9fad142dfc034f0d7a
smarttowel <mihadenok_pavel@mail.ru>
2013-03-22 21:59:24 +0300

    Fix copy-paste mistake

--- source/ORM/mysqladapter.cpp
+++ source/ORM/mysqladapter.cpp
@@ -28,12 +28,10 @@ void MySqlAdapter::fillTableTypes()
     m_tableTypes.insert("qlonglong", "BIGINT");
     m_tableTypes.insert("qulonglong", "BIGINT UNSIGNED");
     m_tableTypes.insert("double", "DOUBLE");
-    m_tableTypes.insert("QDate", "BLOB");
     m_tableTypes.insert("QByteArray", "BLOB");
     m_tableTypes.insert("QChar", "CHAR(1)");
     m_tableTypes.insert("QDate", "DATE");
     m_tableTypes.insert("QTime", "TIME");
     m_tableTypes.insert("QDateTime", "DATETIME");
-    m_tableTypes.insert("QDate", "BLOB");
     m_tableTypes.insert("QString", "TEXT");
 }

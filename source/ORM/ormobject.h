dd06e09265b84a323e0d770932bd7e89e2292d8f
smarttowel <mihadenok_pavel@mail.ru>
2013-03-25 22:12:51 +0300

    Property which declare with macros is private

--- source/ORM/ormobject.h
+++ source/ORM/ormobject.h
@@ -3,8 +3,9 @@
 
 #define ORM_PROPERTY(type, name) \
     Q_PROPERTY(type name READ get##name WRITE set##name) \
-    public: \
+    private: \
     type name; \
+    public: \
     type get##name(){ return name; } \
     void set##name(type input_##name) { name = input_##name; } \
 

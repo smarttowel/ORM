988fb92d6a1e76ebc9147c55512c3a2f7f0fd2aa
smarttowel <mihadenok_pavel@mail.ru>
2013-04-29 00:58:23 +0300

    Add documentation for relations

--- source/lib/macros.h
+++ source/lib/macros.h
@@ -5,9 +5,9 @@
 */
 
 /*!
-  \def ORM_PROPERTY(type, name)
-  Declares property of model. Macros declare private property
-  and public getter and setter.
+   \def ORM_PROPERTY(type, name)
+   Declares property of model. Macros declare private property
+   and public getter and setter.
  */
 
 #define ORM_PROPERTY(type, name) \
@@ -17,6 +17,42 @@
     public: \
     type get##name() const { return name; } \
     void set##name(type input_##name) { name = input_##name; } \
+
+/*!
+   \def ORM_HAS_ONE(ClassName)
+   Declares HAS_ONE relation. Example:
+   \code
+   class DriverLicense : public ORMObject
+   {
+       ...
+   };
+
+   class CarDriver : public ORMObject
+   {
+       ...
+       ORM_HAS_ONE(DriverLicense)
+
+    public:
+       CarDriver() {}
+   };
+   \endcode
+   Macros generate next methods:
+
+   <b>\a ClassName get\a ClassName() \n</b>
+   Returns child \a ClassName, associated with model or 0, if no such child object.
+
+   <b>void set\a ClassName(const \a Classname &object) \n</b>
+   Assigns \a object with model. If the association already exists, this method removes the old and create a new one. \n
+   \b NOTE: \a object must exists in table!
+
+   <b>\a ClassName* create\a ClassName(const QHash<QString, QVariant> &values) \n</b>
+   Creates new \a ClassName object from QHash<fieldName, value> \a values and call save() method. After assigns \a object with model
+   and return pointer to it.
+
+   <b>void remove\a ClassName() \n </b>
+   Removes existing association, but not delete child object from table. If association not exist, does nothing.
+
+ */
 
 #define ORM_HAS_ONE(ClassName) \
     Q_CLASSINFO("HAS_ONE:", #ClassName) \
@@ -70,6 +106,47 @@
         QString whereString = "WHERE parent_id = " + QString::number(id); \
         ORMDatabase::adapter->remove(tableName, whereString); \
     } \
+
+/*!
+   \def ORM_HAS_MANY(ClassName)
+   Declares HAS_MANY relation. Example:
+   \code
+   class Car : public ORMObject
+   {
+       ...
+   };
+
+   class CarDriver : public ORMObject
+   {
+       ...
+       ORM_HAS_ONE(DriverLicense)
+       ORM_HAS_MANY(Car)
+
+    public:
+       CarDriver() {}
+   };
+   \endcode
+   Macros generate next methods:
+
+   <b> QList<\a ClassName*> getAll\a ClassName() \n</b>
+   Returns all \a ClassName, associated with model or empty QList, if no such child object.
+
+   <b>void add\a ClassName(const \a Classname &object) \n</b>
+   Assigns \a object with model.\n
+   \b NOTE: \a object must exists in table!
+
+   <b>\a ClassName* create\a ClassName(const QHash<QString, QVariant> &values) \n</b>
+   Creates new \a ClassName object from QHash<fieldName, value> \a values and call save() method. After assigns \a object with model
+   and return pointer to it.
+
+   <b>QList<\a ClassName*> find<em>ClassName</em>Where(ORMWhere where, ORMGroupBy group = ORMGroupBy(), ORMOrderBy order = ORMOrderBy()) \n</b>
+   Find child objects appropriate given \a where and returns them in accordance with \group and \a order.
+
+   <b>void remove\a ClassName(const \a ClassName &object) \n </b>
+   Removes existing association, but not delete child object from table.\n
+   \b NOTE: \a object must exists in table!
+
+ */
 
 #define ORM_HAS_MANY(ClassName) \
     Q_CLASSINFO("HAS_MANY:", #ClassName) \

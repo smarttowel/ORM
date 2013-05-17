a38ffff8b2ca76c12c088fb02bb9c6ed53d87220
smarttowel <mihadenok_pavel@mail.ru>
2013-05-17 16:26:06 +0300

    Unite all classes, finished main function

--- source/ormtool/main.cpp
+++ source/ormtool/main.cpp
@@ -1,12 +1,22 @@
 #include <QCoreApplication>
-#include <QDebug>
+#include <QStringList>
+#include <QTextStream>
+#include <QFile>
+#include "parser.h"
+#include "sqlscriptbuilder.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug() << argc;
-    for(int i = 0; i < argc; i++)
-        qDebug() << argv[i];
-
+    QStringList files;
+    for(int i = 1; i < argc; i++)
+        files.append(argv[i]);
+    Parser parser;
+    SqlScriptBuilder builder;
+    QFile outputFile("CreateTables.sql");
+    outputFile.open(QIODevice::WriteOnly);
+    QTextStream stream(&outputFile);
+    stream << builder.process(parser.process(files));
+    outputFile.close();
 //    return a.exec();
 }

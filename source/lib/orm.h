0902b40eef8f9767ced88e522c64c5a8f9d4bb23
smarttowel <mihadenok_pavel@mail.ru>
2013-05-28 23:42:21 +0300

    Update orm.h header

--- source/lib/orm.h
+++ source/lib/orm.h
@@ -1,4 +1,13 @@
 #ifndef ORM_H
 #define ORM_H
 
+#include "ormdatabase.h"
+#include "ormobject.h"
+#include "adapters/mysqladapter.h"
+#include "adapters/postgresqladapter.h"
+#include "adapters/sqliteadapter.h"
+#include "ormwhere.h"
+#include "ormgroupby.h"
+#include "ormorderby.h"
+
 #endif // ORM_H
